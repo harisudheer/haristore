a3cdfde139107fc2fa24bb9c3b34f21682d1f9cd
npx <harisudheer@lyrostech.com>
2024-07-09 22:56:13 +0530

    commit

---
+++ components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { AiFillInstagram, AiOutlineTwitter, AiFillFacebook, AiFillYoutube } from 'react-icons/ai'
+
+const Footer = () => {
+  return (
+    <div className='footer-container'>
+        <p>2024 Hari_estore All Rights reserved</p>  
+        <p className='icons'>
+            <AiFillInstagram/>
+            <AiOutlineTwitter/>
+            <AiFillFacebook/>
+            <AiFillYoutube/>
+        </p>    
+    </div>
+  )
+}
+
+export default Footer
